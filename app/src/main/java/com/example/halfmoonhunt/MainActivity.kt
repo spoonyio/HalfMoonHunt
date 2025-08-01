1e0f3c912eee72d511f79a470b412bc0d82a36e2
Andrew Poon <poonan@oregonstate.edu>
2025-07-31 18:38:03 -0700

    added Kotlin implementation of Haversine formula

--- app/src/main/java/com/example/halfmoonhunt/MainActivity.kt
+++ app/src/main/java/com/example/halfmoonhunt/MainActivity.kt
@@ -18,6 +18,9 @@ import com.example.halfmoonhunt.ui.theme.HalfMoonHuntTheme
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
+import kotlin.math.cos
+import kotlin.math.pow
+import kotlin.math.sin
 
 class MainActivity : ComponentActivity() {
 
@@ -95,5 +98,36 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    // Kotlin implementation of the Haversine formula, modified from source:
+    // https://gist.github.com/jferrao/cb44d09da234698a7feee68ca895f491
+    class CalculateDistance(private val lat: Double, private val lon: Double) {
+
+        companion object {
+            const val EARTH_RADIUS_FT: Double = 20903520.0
+        }
+
+        /**
+         * Haversine formula. Giving great-circle distances between two points on a sphere from their longitudes and latitudes.
+         * It is a special case of a more general formula in spherical trigonometry, the law of haversines, relating the
+         * sides and angles of spherical "triangles".
+         *
+         * https://rosettacode.org/wiki/Haversine_formula#Java
+         *
+         * @return Distance in kilometers
+         */
+        fun haversine(destination: CalculateDistance): Double {
+            val dLat = Math.toRadians(destination.lat - this.lat)
+            val dLon = Math.toRadians(destination.lon - this.lon)
+            val originLat = Math.toRadians(this.lat)
+            val destinationLat = Math.toRadians(destination.lat)
+
+            val a = Math.pow(Math.sin(dLat / 2), 2.0) +
+                    Math.pow(Math.sin(dLon / 2), 2.0) * Math.cos(originLat) * Math.cos(destinationLat)
+            val c = 2 * Math.asin(Math.sqrt(a))
+            return EARTH_RADIUS_FT * c
+        }
+
     }
 }
