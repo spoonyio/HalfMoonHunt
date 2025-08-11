986704a2c9c777855782fcfb28b1f75f33793286
Andrew Poon <poonan@oregonstate.edu>
2025-08-10 23:14:56 -0700

    wire up navigation flow with timer and clue screens

--- app/src/main/java/com/example/halfmoonhunt/MainActivity.kt
+++ app/src/main/java/com/example/halfmoonhunt/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.halfmoonhunt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.remember
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.halfmoonhunt.model.SolvedInfo
 import com.example.halfmoonhunt.ui.ClueScreen
+import com.example.halfmoonhunt.ui.CompletedScreen
 import com.example.halfmoonhunt.ui.PermissionsScreen
+import com.example.halfmoonhunt.ui.SolvedClueScreen
 import com.example.halfmoonhunt.ui.StartScreen
 import com.example.halfmoonhunt.ui.theme.HalfMoonHuntTheme
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -25,6 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HalfMoonHuntTheme {
                 val navController = rememberNavController()
+                val huntVm: HuntViewModel = viewModel()
 
                 NavHost(
                     navController = navController,
@@ -33,6 +39,7 @@ class MainActivity : ComponentActivity() {
                     composable("permissions") {
                         PermissionsScreen(
                             onGranted = {
+                                huntVm.loadClues()
                                 navController.navigate("start") {
                                     popUpTo("permissions") { inclusive = true }
                                 }
@@ -42,17 +49,64 @@ class MainActivity : ComponentActivity() {
                     }
                     composable("start") {
                         StartScreen(
-                            onStart = { navController.navigate("clue") }
+                            onStart = {
+                                huntVm.resetHunt()
+                                huntVm.startTimer()
+                                navController.navigate("clue")
+                            }
                         )
                     }
                     composable("clue") {
                         ClueScreen(
+                            huntVm = huntVm,
                             onSolved = {
+                                huntVm.pauseTimer()
+                                navController.navigate("solved")
                             },
                             onQuit = {
+                                huntVm.stopTimer()
                                 navController.navigate("start") {
                                     popUpTo("start") { inclusive = true }
                                 }
+                            }
+                        )
+                    }
+                    composable("solved") {
+                        val clue = remember {
+                            SolvedInfo(
+                                title = "Testn",
+                                body = "You solved the clue!",
+                                facts = "iejwodijwefoij"
+                            )
+                        }
+                        if (clue != null) {
+                            SolvedClueScreen(
+                                solvedClue = clue,
+                                onContinue = {
+                                    val finished = huntVm.advance()
+                                    if (finished) {
+                                        navController.navigate("completed") {
+                                            popUpTo("clue") { inclusive = true }
+                                        }
+                                    } else {
+                                        huntVm.startTimer()
+                                        navController.navigate("clue") {
+                                            popUpTo("clue") { inclusive = true }
+                                        }
+                                    }
+                                }
+                            )
+                        } else {
+                            navController.popBackStack()
+                        }
+                    }
+
+                    composable("completed") {
+                        CompletedScreen(
+                            huntVm = huntVm,
+                            onHome = {
+                                huntVm.stopTimer()
+                                navController.navigate("start") { popUpTo("start") { inclusive = true } }
                             }
                         )
                     }
