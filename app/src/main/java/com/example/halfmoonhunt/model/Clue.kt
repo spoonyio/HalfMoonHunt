88cb697d5651c9554cfa9fd3e67b52df44805978
Andrew Poon <poonan@oregonstate.edu>
2025-08-10 23:44:36 -0700

    make models serializable

--- app/src/main/java/com/example/halfmoonhunt/model/Clue.kt
+++ app/src/main/java/com/example/halfmoonhunt/model/Clue.kt
@@ -1,13 +1,18 @@
 package com.example.halfmoonhunt.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Clue(
     val text: String,
     val hint: String,
     val lat: Double,
     val lon: Double,
-    val threshold: Double
+    val threshold: Double,
+    val solved: SolvedInfo
 )
 
+@Serializable
 data class SolvedInfo(
     val title: String,
     val body: String,
